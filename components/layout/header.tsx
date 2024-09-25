63e941a7b2a7d3ae15769fcad4eb20aac8ae6f21
thorski1 <sthoyre@gmail.com>
2024-09-25 14:29:28 -0400

    Update: make header absolute instead of fixed

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -17,7 +17,7 @@ const Header = () => {
   };
 
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-foreground/50 backdrop-blur-sm shadow-sm">
+    <header className="absolute top-0 left-0 right-0 z-50 bg-foreground/50 backdrop-blur-sm shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {!isMenuOpen && (<div className="flex justify-between items-center py-2 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
