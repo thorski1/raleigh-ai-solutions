cf918c1789ccad75ccb6c95f45eeafc7765b1f4f
thorski1 <sthoyre@gmail.com>
2024-09-25 14:02:58 -0400

    Further updates to navbar with new logo

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import Image from 'next/image';
+import Image from 'next/image'
 import { useState } from 'react';
 import { Menu, X } from 'lucide-react';
 
