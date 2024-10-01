f382fb46abe66e13d8e7ff10191c33dc2d6f6ea1
thorski1 <sthoyre@gmail.com>
2024-10-01 10:42:38 -0400

    Add Google Analytics and Google Tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Header from '../components/layout/header';
 import Footer from '../components/layout/footer';
 import { cn } from '@/lib/utils';
+import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,11 +20,15 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <GoogleTagManager gtmId="GTM-MGXD95K8" />
+      </head>
       <body className={cn(inter.className, 'overflow-x-hidden max-w-full')}>
         <Header />
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-9KL88EYGE4" />
     </html>
   );
 }
