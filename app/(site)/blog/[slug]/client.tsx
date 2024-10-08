10e8f2fc879398ed533cc299bdd7e858df6c5532
thorski1 <sthoyre@gmail.com>
2024-10-07 20:46:34 -0400

    Add SEO improvements for blog

---
+++ app/(site)/blog/[slug]/client.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import Image from 'next/image'
+import { urlFor } from '@/sanity/lib/image'
+import { PortableText } from '@portabletext/react'
+import AnimatedGridPattern from '@/components/magicui/animated-grid-pattern'
+import { portableTextComponents } from '@/lib/portable-text.config'
+
+type BlogPostPageClientProps = {
+  post: any // Replace 'any' with the actual type of your post
+}
+
+export default function BlogPostPageClient({ post }: BlogPostPageClientProps) {
+  return (
+    <article>
+      <div className="px-8 md:px-16 lg:px-24 py-8 flex flex-col items-center justify-center md:py-16 lg:py-20 pt-28 md:pt-32 lg:pt-36 bg-gradient-to-br from-secondary via-secondary-foreground to-primary text-white">
+        <AnimatedGridPattern className="absolute inset-0 z-0 opacity-40" />
+
+        <h1 className="text-4xl font-bold mb-4 text-center">{post.title}</h1>
+        <div className="mb-4 flex font-bold items-center text-white">
+          <span>{new Date(post.publishedAt).toLocaleDateString()}</span>
+          <span className="mx-2">•</span>
+          <span>{post.estimatedReadingTime} min read</span>
+        </div>
+      </div>
+      <div className="container mx-auto px-4 py-8">
+        {post.mainImage && (
+          <Image
+            src={urlFor(post.mainImage).url()}
+            alt={post.title}
+            width={800}
+            height={400}
+            className="w-full relative z-10 h-auto mb-8 rounded-lg -mt-16 md:-mt-24"
+          />
+        )}
+        <div className="prose max-w-none">
+          <PortableText
+            value={post.content}
+            // @ts-ignore
+            components={portableTextComponents}
+          />
+        </div>
+        <div className="mt-8">
+          <h2 className="text-2xl font-semibold mb-2">Author</h2>
+          <div className="flex items-center">
+            {post.author.image && (
+              <Image
+                src={urlFor(post.author.image).width(50).height(50).url()}
+                alt={post.author.name}
+                width={50}
+                height={50}
+                className="rounded-full mr-4"
+              />
+            )}
+            <span className="text-lg">{post.author.name}</span>
+          </div>
+        </div>
+        <div className="mt-8">
+          <h2 className="text-2xl font-semibold mb-2">Categories</h2>
+          <div className="flex flex-wrap gap-2">
+            {post.categories.map((category: string) => (
+              <span key={category} className="bg-gray-200 px-3 py-1 rounded-full text-sm">
+                {category}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </article>
+  )
+}
--- app/(site)/blog/[slug]/page.tsx
+++ app/(site)/blog/[slug]/page.tsx
@@ -1,72 +1,62 @@
-import { trpc } from '@/trpc/server';
-import Image from 'next/image';
-import { urlFor } from '@/sanity/lib/image';
-import { PortableText, PortableTextMarkComponentProps } from '@portabletext/react';
-import AnimatedGridPattern from '@/components/magicui/animated-grid-pattern';
-import { portableTextComponents } from '@/lib/portable-text.config';
+import { Metadata, ResolvingMetadata } from 'next'
+import { trpc } from '@/trpc/server'
+import BlogPostPageClient from './client'
+import { urlFor } from '@/sanity/lib/image'
 
-export default async function BlogPostPage({ params }: { params: { slug: string } }) {
-  const post = await trpc.getPostBySlug({ slug: params.slug });
+type Props = {
+  params: { slug: string }
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const post = await trpc.getPostBySlug({ slug: params.slug })
 
   if (!post) {
-    return <div className="container mx-auto px-4 py-8">Post not found</div>;
+    return {
+      title: 'Post Not Found',
+    }
   }
 
-  return (
-    <article>
-      <div className="px-8 md:px-16 lg:px-24 py-8 flex flex-col items-center justify-center md:py-16 lg:py-20 pt-28 md:pt-32 lg:pt-36 bg-gradient-to-br from-secondary via-secondary-foreground to-primary text-white">
-        <AnimatedGridPattern className="absolute inset-0 z-0 opacity-40" />
+  const previousImages = (await parent).openGraph?.images || []
 
-        <h1 className="text-4xl font-bold mb-4 text-center">{post.title}</h1>
-        <div className="mb-4 flex font-bold items-center text-white">
-          <span>{new Date(post.publishedAt).toLocaleDateString()}</span>
-          <span className="mx-2">•</span>
-          <span>{post.estimatedReadingTime} min read</span>
-        </div>
-      </div>
-      <div className="container mx-auto px-4 py-8">
-        {post.mainImage && (
-          <Image
-            src={urlFor(post.mainImage).url()}
-            alt={post.title}
-            width={800}
-            height={400}
-            className="w-full relative z-10 h-auto mb-8 rounded-lg -mt-16  md:-mt-24"
-          />
-        )}
-        <div className="prose max-w-none">
-          <PortableText
-            value={post.content}
-            // @ts-ignore
-            components={portableTextComponents}
-          />
-        </div>
-        <div className="mt-8">
-          <h2 className="text-2xl font-semibold mb-2">Author</h2>
-          <div className="flex items-center">
-            {post.author.image && (
-              <Image
-                src={urlFor(post.author.image).width(50).height(50).url()}
-                alt={post.author.name}
-                width={50}
-                height={50}
-                className="rounded-full mr-4"
-              />
-            )}
-            <span className="text-lg">{post.author.name}</span>
-          </div>
-        </div>
-        <div className="mt-8">
-          <h2 className="text-2xl font-semibold mb-2">Categories</h2>
-          <div className="flex flex-wrap gap-2">
-            {post.categories.map((category: any) => (
-              <span key={category} className="bg-gray-200 px-3 py-1 rounded-full text-sm">
-                {category}
-              </span>
-            ))}
-          </div>
-        </div>
-      </div>
-    </article>
-  );
+  return {
+    title: `${post.title} | Raleigh AI Solutions Blog`,
+    description: post.excerpt,
+    openGraph: {
+      title: `${post.title} | Raleigh AI Solutions Blog`,
+      description: post.excerpt,
+      url: `https://www.raleighai.solutions/blog/${post.slug.current}`,
+      siteName: 'Raleigh AI Solutions',
+      images: [
+        {
+          url: post.mainImage ? urlFor(post.mainImage).width(1200).height(630).url() : previousImages[0],
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+      locale: 'en_US',
+      type: 'article',
+      publishedTime: post.publishedAt,
+      authors: [post.author.name],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: `${post.title} | Raleigh AI Solutions Blog`,
+      description: post.excerpt,
+      images: [post.mainImage ? urlFor(post.mainImage).width(1200).height(630).url() : previousImages[0]],
+    },
+  }
+}
+
+export default async function BlogPostPage({ params }: Props) {
+  const post = await trpc.getPostBySlug({ slug: params.slug })
+
+  if (!post) {
+    return <div className="container mx-auto px-4 py-8">Post not found</div>
+  }
+
+  return <BlogPostPageClient post={post} />
 }
